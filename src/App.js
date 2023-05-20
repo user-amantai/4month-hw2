eb5fc8d80b77682ceecdc339963c0d85a0703c7e
amantai <949898ai@gmail.com>
2023-05-20 11:27:59 +0600

    done hw2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useState } from 'react';
+
+let Square = () => {
+  const [colorA, setColorA] = useState('red')
+  const [colorB, setColorB] = useState('blue')
+  let renderColors = () => {
+    setColorA(colorA === 'red' ? 'blue' : 'red');
+    setColorB(colorB === 'red' ? 'blue' : 'red');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:'flex', justifyContent:'center', alignItems:'center',gap:'20px', backgroundColor: 'teal', height:'120px',maxWidth:'320px'}}>
+      <div style={{ width: '100px', height: '100px', backgroundColor: colorA, borderRadius: '50px'}}
+        onClick={renderColors}></div>
+      <div style={{ width: '100px', height: '100px', backgroundColor: colorB, borderRadius: '50px'}}
+        onClick={renderColors}></div>
     </div>
   );
-}
+};
 
-export default App;
+export default Square;
